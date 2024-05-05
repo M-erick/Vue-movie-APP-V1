ef96e0305cc12a9982d47f29d376e3699d0f1155
M-erick <muriithierick3758@gmail.com>
2024-05-05 15:43:53 +0300

    default create view page for admin side

--- movie-app/src/views/admin/Create.vue
+++ movie-app/src/views/admin/Create.vue
@@ -1,13 +1,27 @@
-<!-- CreateMovie.vue -->
 <template>
   <div>
     <h2>Create Movie</h2>
-    <form @submit.prevent="createMovie">
-      <input v-model="formData.title" placeholder="Title">
-      <input v-model="formData.description" placeholder="Description">
-      <input v-model="formData.genre" placeholder="Genre">
-      <input type="date" v-model="formData.releaseDate" placeholder="Release Date">
-      <input type="number" v-model.number="formData.rating" min="1" max="10" placeholder="Rating">
+    <form @submit.prevent="updateMovie" class="movie-form">
+      <div class="form-group">
+        <label for="title">Title:</label>
+        <input type="text" id="title" v-model="formData.title" placeholder="Title">
+      </div>
+      <div class="form-group">
+        <label for="description">Description:</label>
+        <textarea id="description" v-model="formData.description" placeholder="Description"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="genre">Genre:</label>
+        <input  type="text" id="genre" v-model="formData.genre" placeholder="Genre">
+      </div>
+      <div class="form-group">
+        <label for="releaseDate">Release Date:</label>
+        <input type="date" id="releaseDate" v-model="formData.releaseDate" placeholder="Release Date">
+      </div>
+      <div class="form-group">
+        <label for="rating">Rating:</label>
+        <input type="number" id="rating" v-model.number="formData.rating" min="1" max="10" placeholder="Rating">
+      </div>
       <button type="submit">Create</button>
     </form>
   </div>
@@ -17,6 +31,7 @@
 import { ref } from 'vue';
 import axios from 'axios';
 
+const movieId = ref(null);
 const formData = ref({
   title: '',
   description: '',
@@ -25,15 +40,65 @@ const formData = ref({
   rating: null
 });
 
-async function createMovie() {
+async function updateMovie() {
   try {
-    await axios.post('http://localhost:3000/api/movies', formData.value);
-    // Optionally, you can redirect to the movies list page or show a success message
-    setTimeout(() => {
-      router.push('/movies'); // Redirect to the movies list page
-    }, 2000); // Redirect after 2 seconds (2000 milliseconds)
+    await axios.post(`http://localhost:3000/api/movies/${movieId.value}`, formData.value);
+  //  Redirect to the movies list page after 2 seconds
+    setTimeout(()=>{
+      router.push('/movies');
+    },2000);
   } catch (error) {
-    console.error('Failed to create movie:', error);
+    console.error('Failed to update movie:', error);
   }
 }
 </script>
+
+<style scoped>
+h2{
+  text-align: center;
+  padding-top: 10px;
+  padding-bottom:2px;
+}
+.movie-form {
+  max-width: 400px;
+  margin: 30px auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input[type="text"],
+input[type="number"],
+textarea,
+input[type="date"] {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+button {
+  display: block;
+  background-color: crimson;
+  color: #fff;
+  border: none;
+  padding:10px;
+  border-radius: 4px;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+</style>
