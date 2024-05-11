da6081a2de318c5c2ddfba93913d3155490a68ed
M-erick <muriithierick3758@gmail.com>
2024-05-11 12:09:22 +0300

    commits

--- movie-app/tailwind.config.js
+++ movie-app/tailwind.config.js
@@ -1,17 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './public/**/*.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}'
-],
-  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
-
